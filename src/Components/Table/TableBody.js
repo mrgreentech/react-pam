22a6d4633320fa904b6d7e5ab560224a83d0ab4d
Mikael Pettersson <mikael.pettersson@mrgreen.com>
2018-02-12 14:34:21 +0100

    gitignore dist and storybook-static folder

--- src/Components/Table/TableBody.js
+++ src/Components/Table/TableBody.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import childrenOf from '../../Utility/childrenOf'
 import TableRow from './TableRow';
 
 const TableBody = ({children, ...rest}) => <tbody {...rest}>{children}</tbody>;
